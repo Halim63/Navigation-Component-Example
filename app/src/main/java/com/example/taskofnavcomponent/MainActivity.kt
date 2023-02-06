2a688b5c1e749495dc82c6633051472132fd79b6
Halim <lemohalem31@gmail.com>
2023-02-06 17:16:19 +0200

    NavigationComponent

--- app/src/main/java/com/example/taskofnavcomponent/MainActivity.kt
+++ app/src/main/java/com/example/taskofnavcomponent/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.taskofnavcomponent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -43,6 +45,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d(MainActivity::class.java.simpleName,"onNewIntent: ")
+        navController.handleDeepLink(intent)
     }
 
 }
